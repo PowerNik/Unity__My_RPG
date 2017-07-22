59d008f417e394b3adbded35232b0f9447ac52e2
nmaschinskiy <nmaschinskiy@mail.ru>
2017-07-22 17:23:33 +0400

    Ghange BulletLifetime.script logic

--- Assets/Penguin/Scripts/BulletLifetime.cs
+++ Assets/Penguin/Scripts/BulletLifetime.cs
@@ -5,21 +5,9 @@ using UnityEngine;
 public class BulletLifetime : MonoBehaviour
 {
 	public float LifeTime = 2f;
-	private float Timer;
 
 	void Start()
 	{
-		Timer = LifeTime;
-	}
-
-	// Update is called once per frame
-	void Update()
-	{
-		Timer = (Timer - Time.deltaTime > 0) ? Timer - Time.deltaTime : 0;
-		if (Timer == 0)
-		{
-			Timer = LifeTime;
-			Destroy(gameObject);
-		}
+		Destroy(gameObject, LifeTime);
 	}
 }
