92b95440ed48f9250fb78d0ec024d369be80f029
nmaschinskiy <nmaschinskiy@mail.ru>
2017-07-22 21:10:06 +0400

    Modify cameraController to adequate state

--- Assets/Penguin/Scripts/CameraController.cs
+++ Assets/Penguin/Scripts/CameraController.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-	public Transform playerPosition;
+	public Transform targetPosition;
 
-	float scrollSpeed = 5f;
+	float scrollSpeed = 1f;
 	float sensX = 3f;
 	float sensY = 3f;
 
 	const float MIN_CAMERA_DISTANCE = 3f;
-	const float MAX_CAMERA_DISTANCE = 15f;
-	const float MIN_ANGLE_Y = 0;
+	const float MAX_CAMERA_DISTANCE = 10f;
+	const float MIN_ANGLE_Y = -10;
 	const float MAX_ANGLE_Y = 90;
 
 	void LateUpdate()
@@ -24,23 +24,28 @@ public class CameraController : MonoBehaviour
 
 	void MoveCamera()
 	{
-		if (Input.GetKey(KeyCode.L))
-			Debug.Break();
-
 		float newAngleY = -Input.GetAxis("Mouse Y") * sensY + transform.rotation.eulerAngles.x;
+		if (newAngleY > 270)
+			newAngleY -= 360;
 
-		if (MIN_ANGLE_Y < newAngleY && newAngleY < MAX_ANGLE_Y)
-			transform.RotateAround(playerPosition.transform.position, transform.right, -Input.GetAxis("Mouse Y") * sensY);
+		if(MIN_ANGLE_Y <= newAngleY && newAngleY <= MAX_ANGLE_Y)
+			transform.RotateAround(targetPosition.transform.position, transform.right,
+				-Input.GetAxis("Mouse Y") * sensY);
 
-		transform.RotateAround(playerPosition.transform.position, Vector3.up, Input.GetAxis("Mouse X") * sensX);
+		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) ||
+		Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+			targetPosition.rotation = targetPosition.rotation *
+				Quaternion.Euler(0, Input.GetAxis("Mouse X") * sensX, 0);
+		else
+			transform.RotateAround(targetPosition.transform.position, Vector3.up,
+										Input.GetAxis("Mouse X") * sensX);
 	}
 
 	void ScrollZoom()
 	{
-		Vector3 newPos = transform.localPosition +
-			transform.forward * Input.GetAxis("Mouse ScrollWheel") * scrollSpeed;
+		Vector3 newPos = transform.localPosition * (1 - Input.GetAxis("Mouse ScrollWheel") * scrollSpeed);
 
-		if (MIN_CAMERA_DISTANCE < -newPos.z && -newPos.z < MAX_CAMERA_DISTANCE)
+		if (MIN_CAMERA_DISTANCE < newPos.magnitude && newPos.magnitude < MAX_CAMERA_DISTANCE)
 			transform.localPosition = newPos;
 	}
 }
