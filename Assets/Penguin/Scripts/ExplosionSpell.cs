264e71522e1fc02766687324ae6c59db190aae1f
nmaschinskiy <nmaschinskiy@mail.ru>
2017-07-25 13:34:35 +0400

    Added MakeAnAttraction() in ExplosionSpell.cs

--- Assets/Penguin/Scripts/ExplosionSpell.cs
+++ Assets/Penguin/Scripts/ExplosionSpell.cs
@@ -6,13 +6,15 @@ public class ExplosionSpell : MonoBehaviour
 {
 	public float power = 15f;
 	public float radius = 10f;
-	public float upForce = 2f;
+	public float upForce = 3f;
 
 
 	void FixedUpdate()
 	{
 		if (Input.GetKeyDown(KeyCode.Mouse1))
 			MakeAnExplosion();
+		if (Input.GetKeyDown(KeyCode.Mouse2))
+			MakeAnAttraction();
 	}
 
 	void MakeAnExplosion()
@@ -28,6 +30,22 @@ public class ExplosionSpell : MonoBehaviour
 			Rigidbody rb = col.GetComponent<Rigidbody>();
 			if (rb != null)
 				rb.AddExplosionForce(power, explosionPosition, radius, upForce, ForceMode.Impulse);
+		}
+	}
+
+	void MakeAnAttraction(float power = 15f, float radius = 25f, float upForce = 1f)
+	{
+		Vector3 explosionPosition = transform.position;
+		Collider[] colliders = Physics.OverlapSphere(explosionPosition, radius);
+
+		foreach (Collider col in colliders)
+		{
+			if (col.tag == "Player")
+				continue;
+
+			Rigidbody rb = col.GetComponent<Rigidbody>();
+			if (rb != null)
+				rb.AddExplosionForce(-power, explosionPosition, radius, -upForce, ForceMode.Impulse);
 		}
 	}
 }
