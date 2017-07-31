a9b10bf3e826245be5a0f89a3b571cc8e7ce54c6
nmaschinskiy <nmaschinskiy@mail.ru>
2017-07-31 23:04:22 +0400

    Add LazerBullet

---
+++ Assets/Packets/Scripts/LazerBullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Packets
+{
+	public class LazerBullet : AbstractBullet
+	{
+		Collider other;
+
+		public override float Damage { get { return 10; } }
+		public override float Mass { get { return 0.1f; } }
+		public override float Speed { get { return 50; } }
+
+		private void OnTriggerEnter(Collider _other)
+		{
+			other = _other;
+
+			DoAttack();
+			Destroy(gameObject);
+		}
+
+		public override void DoAttack()
+		{
+			if (other.gameObject.tag == "Enemy")
+				Destroy(other.gameObject);
+		}
+	}
+}
