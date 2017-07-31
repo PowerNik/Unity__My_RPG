918ca01de5cf0c7a7dde18a32cbd1c2250089a04
nmaschinskiy <nmaschinskiy@mail.ru>
2017-08-01 00:06:54 +0400

    Create KernelParaboleBullet

---
+++ Assets/Packets/Scripts/KernelBullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Packets
+{
+	public class KernelBullet : AbstractBullet
+	{
+		Collider other;
+
+		public override float Damage { get { return 20; } }
+		public override float Mass { get { return 5; } }
+		public override float Speed { get { return 100; } }
+
+		private void OnTriggerEnter(Collider _other)
+		{
+			other = _other;
+
+			DoAttack();
+			Destroy(gameObject);
+		}
+
+		public override void DoAttack()
+		{
+			if (other.gameObject.tag == "Enemy")
+				Destroy(other.gameObject);
+		}
+	}
+}
