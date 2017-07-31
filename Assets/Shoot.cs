f88c91c661ca365780412e3698da1a81cf5b35f4
nmaschinskiy <nmaschinskiy@mail.ru>
2017-07-31 22:21:52 +0400

    Add metaInfo

---
+++ Assets/Shoot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Packets;
+
+public class Shoot : MonoBehaviour
+{
+	public Transform CameraPos;
+	public Transform SpawnPos;
+	RaycastHit HIT;
+	public GameObject packet;
+
+	void ShootMe()
+	{
+		Vector3 startPos = SpawnPos.position;
+		GameObject T;
+
+		Physics.Raycast(CameraPos.position, CameraPos.forward, out HIT);
+		Vector3 direction = (HIT.point - startPos).normalized;
+
+		T = Instantiate(packet, startPos, Quaternion.Euler(direction));
+		T.GetComponent<AbstractMover>().Move(startPos, direction);
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonUp(0))
+			ShootMe();
+	}
+}
