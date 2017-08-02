f35739d3b3264be74a6a0705e17b97439791f232
nmaschinskiy <nmaschinskiy@mail.ru>
2017-08-03 01:34:41 +0400

    Create and fill Packet SO, enum and Settings

---
+++ Assets/Scripts/ScriptableObjects/PacketScriptableObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "Packet", menuName = "My Scriptable Objects/Packet Settings", order = 3)]
+public class PacketScriptableObject : ScriptableObject
+{
+	public Packet[] packets;
+}
+
+[System.Serializable]
+public class Packet
+{
+	public PacketType type;
+	public GameObject prefab;
+
+	public float damage = 10;
+	public float speed = 50;
+	public float mass = 0.01f;
+}
