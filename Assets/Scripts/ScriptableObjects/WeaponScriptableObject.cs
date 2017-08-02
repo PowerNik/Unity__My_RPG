4c301828411e826b588e6310b53fcc6f33b8907a
nmaschinskiy <nmaschinskiy@mail.ru>
2017-08-03 01:34:01 +0400

    Fill Weapon Settings

--- Assets/Scripts/ScriptableObjects/WeaponScriptableObject.cs
+++ Assets/Scripts/ScriptableObjects/WeaponScriptableObject.cs
@@ -10,9 +10,21 @@ public class WeaponScriptableObject : ScriptableObject
 [System.Serializable]
 public class Weapon
 {
-	public WeaponType type;
-	public Texture icon;
-	public int particle;
+	public string weaponName = "My Gun";
+	public WeaponType type = WeaponType.LaserGun;
+
 	public GameObject prefab;
-	public GameObject bullet;
+	public Texture icon;
+
+	public BulletType bulletType = BulletType.LinearLaserBullet;
+
+	public bool isAutomatic = false;
+	public bool isCollimator = false;
+
+	[Range(1, 1000)]
+	public int Capacity = 100;
+	[Range(0, 10)]
+	public float coolDown = 1f;
+	[Range(0, 45)]
+	public float dispersionAngle = 10;
 }
